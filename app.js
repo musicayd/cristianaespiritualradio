70fed40df1d9e374650b1cf545c40e244bd3b363
radioc <ydproyectosrz398@gmail.com>
2026-05-22 17:07:17 -0600

    Update app.js

--- app.js
+++ app.js
@@ -366,7 +366,7 @@ if(savedSong !== null){
 
 }else{
 
-    currentSong = songs.findIndex(song => song.file === "yoteseguire.mp3");
+    currentSong = Math.floor(Math.random() * songs.length);
 }
 
 audio.src = songs[currentSong].file;
