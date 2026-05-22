52bec84444880dc902571d6d8923a4c59fb9ccf8
radioc <ydproyectosrz398@gmail.com>
2026-05-22 15:59:12 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,59 @@
-const CACHE_NAME = "radio-cache-v1";
-const assets = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./app.js",
-  "./manifest.json",
-  "./cover.png",
-  "./icon.png"
+const CACHE_NAME = "radio-cache-v2";
+
+const urlsToCache = [
+
+    "./",
+    "./index.html",
+    "./style.css",
+    "./app.js",
+    "./manifest.json",
+    "./icon.png",
+    "./portada.png",
+    "./intro.jpeg"
 ];
 
-// Instalar el Service Worker y guardar los archivos en la memoria
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assets);
-    })
-  );
+/* =========================
+   FORCE UPDATE
+========================= */
+
+self.addEventListener("install", () => {
+
+    self.skipWaiting();
 });
 
-// Responder con los archivos guardados si no hay internet
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("activate", (event) => {
+
+    event.waitUntil(clients.claim());
+});
+
+/* =========================
+   CACHE FILES
+========================= */
+
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+            .then(cache => {
+
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+/* =========================
+   FETCH
+========================= */
+
+self.addEventListener("fetch", event => {
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then(response => {
+
+                return response || fetch(event.request);
+            })
+    );
 });
