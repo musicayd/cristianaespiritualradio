37e522d5b73fad6f257c896ec11599f7b9efc369
radioc <ydproyectosrz398@gmail.com>
2026-07-02 21:40:28 -0600

    Update app.js

--- app.js
+++ app.js
@@ -642,27 +642,7 @@ audio.addEventListener("ended", () => {
 });
 
 
-/* CAMBIAR PORTADA AUTOMATICA */
 
-if(songs[currentSong].file === "Saludo.mp3"){
-
-    coverImage.src = "comerciales.jpeg";
-
-}else if(songs[currentSong].file === "yoteseguire.mp3"){
-
-    coverImage.src = "yoteseguire.jpg";
-
-}else if(songs[currentSong].file === "laobramaravillosa.mp3"){
-
-    coverImage.src = "laobramaravillosa.jpeg";
-
-}else{
-
-    coverImage.src = "portada.jpg";
-}
-
-audio.play();
-});
 
 /* RANDOM SIN REPETIR */
 
