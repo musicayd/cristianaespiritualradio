3b673e4894edee5d1d1b0cf6aaef5fe9643422e7
radioc <ydproyectosrz398@gmail.com>
2026-08-13 15:40:54 -0600

    Update app.js

--- app.js
+++ app.js
@@ -143,7 +143,7 @@ const songs = [
 
     { title: "📻 Radio", file: "Saludo.mp3" },
 
-    { title: "Proverbios 3:5-6", file: "Oracion.mp3" },
+    { title: "MicroMensajes", file: "Oracion.mp3" },
 
     { title: "Calles de oro", file: "CallesDeOro.mp3" },
 
